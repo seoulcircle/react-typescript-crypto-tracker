23396a81a6b69872a1e7c7661e43a22eba3142b4
seoulcircle <77052964+seoulcircle@users.noreply.github.com>
2025-02-19 01:17:38 +0900

    COINS api 리스트

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,8 +1,78 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const Container = styled.div`
+  padding: 20px;
+  margin: 0 auto;
+  max-width: 300px;
+`;
+
+const Header = styled.header`
+  height: 5vh;
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const CoinList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: ${(props) => props.theme.textColor};
+  color: ${(props) => props.theme.bgColor};
+  margin: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  a {
+    display: block;
+    padding: 10px;
+    transition: color 0.2s;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coincap.io/v2/assets");
+      const json = await response.json();
+      setCoins(json.data);
+      setLoading(false);
+    })();
+  }, []);
+
   return (
-    <div>
-      <h1>Coins</h1>
-    </div>
+    <Container>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      {loading ? (
+        "LOADING..."
+      ) : (
+        <CoinList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`${coin.id}`}>{coin.name} &rarr;</Link>
+            </Coin>
+          ))}
+        </CoinList>
+      )}
+    </Container>
   );
 }
 
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "blue",
+  bgColor: "#353b48",
+  textColor: "#f5f6fa",
+  accentColor: "#e84118",
 };
